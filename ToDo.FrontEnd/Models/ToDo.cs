988df43ffcae9fd6506bf6586de0ba753942ba52
Sam Aripov <username@users.noreply.github.com>
2025-11-06 20:22:00 -0500

    Moved the todo item view into a component with it's styles.

--- ToDo.FrontEnd/Models/ToDo.cs
+++ ToDo.FrontEnd/Models/ToDo.cs
@@ -4,10 +4,10 @@ namespace ToDo.FrontEnd.Models;
 
 public class ToDo
 {
-    public int Id { get; set; }
-    public required string Title { get; set; }
-    public string Description { get; set; } = string.Empty;
-    public bool Complete { get; set; } = false;
-    public readonly DateTime CreatedAt = DateTime.Now;
-    public DateTime CompletedAt { get; set; }
+  public int Id { get; set; }
+  public required string Title { get; set; }
+  public string Description { get; set; } = string.Empty;
+  public bool Complete { get; set; } = false;
+  public readonly DateTime CreatedAt = DateTime.Now;
+  public DateTime CompletedAt { get; set; }
 }
