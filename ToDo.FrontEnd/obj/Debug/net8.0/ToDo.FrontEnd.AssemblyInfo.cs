2d525fdc9c65721d9d21103c8c46983164951152
Sam Aripov <username@users.noreply.github.com>
2025-11-08 19:32:07 -0500

    Updated and smoothed the styles

--- ToDo.FrontEnd/obj/Debug/net8.0/ToDo.FrontEnd.AssemblyInfo.cs
+++ ToDo.FrontEnd/obj/Debug/net8.0/ToDo.FrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo.FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738b926fbf12449ff06709fc679b1e3ec9c8c5d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f92314fd9e6a5696f65205b4a8174357b98ecd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo.FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo.FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
